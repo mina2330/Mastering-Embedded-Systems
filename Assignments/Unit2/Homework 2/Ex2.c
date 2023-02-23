1d51721c626e18a94266920451131342e4e08de0
mina2330 <124838825+mina2330@users.noreply.github.com>
2023-02-23 17:12:25 +0200

    Added Ex2.c

---
+++ Assignments/Unit2/Homework 2/Ex2.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 12, 2023
+ *      Author: Mina
+ */
+#include<stdio.h>
+
+int main(void)
+{
+	char a;
+	printf("Enter an alphabet: ");
+	fflush(stdout);fflush(stdin);
+	scanf("%c",&a);
+
+
+switch (a)
+	{case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u':
+		printf("%c is a vowel",a);
+		break;
+	default:
+		printf("%c is a consonant.",a);
+	}
+
+return 0;
+
+}
+
+
